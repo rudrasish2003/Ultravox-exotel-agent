f3b8fd4442d87f41bc387ad032c0554d233b606d
Rudrasish <rudrasish2003@gmail.com>
2025-07-02 19:03:54 +0530

    Updated to common js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import axios from 'axios';
-import dotenv from 'dotenv';
-import https from 'https';
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const dotenv = require('dotenv');
+const https = require('https');
 const cheerio = require('cheerio');
-import { GoogleGenerativeAI } from '@google/generative-ai';
-
+const { GoogleGenerativeAI } = require('@google/generative-ai');
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
